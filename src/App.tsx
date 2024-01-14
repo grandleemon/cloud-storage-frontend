c5252b4c63d4561967ba03be8c80552dbf034430
Maxim <zeetreex@gmail.com>
2024-01-14 13:59:19 +0200

    base styles to look correct

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import styles from "./App.module.scss";
 import axios from "axios";
 
-const extensions = ["msi", "exe"];
+const allowedExtensions = ["msi", "exe"];
 
 const getFiles = async (): Promise<string[]> => {
   const { data } = await axios.get<string[]>("http://localhost:3000/api/files");
@@ -10,8 +11,10 @@ const getFiles = async (): Promise<string[]> => {
 };
 
 function App() {
+  const [notifications, setNotifications] = useState<string[]>([]);
   const [allFiles, setAllFiles] = useState<string[]>();
-  const [file, setFile] = useState<File>();
+  const [file, setFile] = useState<File | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const onUpload = (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -32,8 +35,16 @@ function App() {
         },
       });
 
+      if (file) setNotifications(prev => [...prev, file?.name]);
+
+      setTimeout(() => {
+        setNotifications([]);
+      }, 4000);
+
       const files = await getFiles();
       setAllFiles(files);
+      setFile(null);
+      if (inputRef.current) inputRef.current.value = "";
     } catch (e) {
       console.error(e);
     }
@@ -46,20 +57,40 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <input type="file" onChange={onUpload} />
+    <>
+      <input ref={inputRef} type="file" onChange={onUpload} />
       <button disabled={!file} onClick={uploadFile}>Upload</button>
-      {allFiles?.map((fileName, i) => {
-        const split = fileName.split(".");
-        const extension = split[split.length - 1];
+      {allFiles
+        ? <div className={styles.files}>
+          {allFiles.map((fileName, i) => {
+            const split = fileName.split(".");
+            const extension = split[split.length - 1];
 
-        return <div key={i}>
-          {fileName}
-          {extensions.includes(extension) &&
-            <a download href={`http://localhost:3000/downloads/${fileName}`}>Download</a>}
-        </div>;
-      })}
-    </div>
+            return <div className={styles.file} key={i}>
+              <span>{fileName}</span>
+              {allowedExtensions.includes(extension) &&
+                <>
+                  <span>Download</span>
+                  <a className={styles.downloadLink}
+                     href={`http://localhost:3000/downloads/${fileName}`}
+                  />
+                </>
+              }
+            </div>;
+          })}
+        </div>
+        : null
+      }
+
+      {notifications
+        ? <div className={styles.notifications}>
+          {notifications.map(item => {
+            return <div className={styles.notification}>{item} was uploaded successfully!</div>;
+          })}
+        </div>
+        : null
+      }
+    </>
   );
 }
 
